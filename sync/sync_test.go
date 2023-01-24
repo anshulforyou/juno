733bf755466bd699f59e303e223ad2a343ec6d29
IronGauntlets <aneeque@nethermind.io>
2023-01-24 20:59:56 +0000

    Create skeleton for synchronizer tests

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,6 +1,7 @@
 package sync
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/NethermindEth/juno/blockchain"
@@ -30,26 +31,50 @@ Todo:
 
 func TestSyncBlocks(t *testing.T) {
 	bc := blockchain.NewBlockchain()
-	fakeData := &fakeStarkNetData{}
+	fakeData := newFakeStarkNetData()
 	synchronizer := NewSynchronizer(bc, fakeData)
 	err := synchronizer.SyncBlocks()
 	assert.NoError(t, err)
 }
 
-type fakeStarkNetData struct{}
+type fakeStarkNetData struct {
+	blocks      map[uint64]*core.Block
+	stateUpdate map[uint64]*core.StateUpdate
+}
+
+func newFakeStarkNetData() *fakeStarkNetData {
+	blocksM, stateUpdateM := populateBlocksAndStateUpdate()
+	return &fakeStarkNetData{blocksM, stateUpdateM}
+}
+
+func populateBlocksAndStateUpdate() (map[uint64]*core.Block, map[uint64]*core.StateUpdate) {
+	// go
+	bm := make(map[uint64]*core.Block)
+	sm := make(map[uint64]*core.StateUpdate)
+
+	return bm, sm
+}
 
 func (f *fakeStarkNetData) BlockByNumber(blockNumber uint64) (*core.Block, error) {
-	return nil, nil
-}
-
-func (f *fakeStarkNetData) Transaction(transactionHash *felt.Felt) (*core.Transaction, error) {
-	return nil, nil
-}
-
-func (f *fakeStarkNetData) Class(classHash *felt.Felt) (*core.Class, error) {
-	return nil, nil
+	b := f.blocks[blockNumber]
+	if b == nil {
+		return nil, errors.New("unknown block")
+	}
+	return b, nil
 }
 
 func (f *fakeStarkNetData) StateUpdate(blockNumber uint64) (*core.StateUpdate, error) {
+	u := f.stateUpdate[blockNumber]
+	if u == nil {
+		return nil, errors.New("unknown block")
+	}
+	return u, nil
+}
+
+func (f *fakeStarkNetData) Transaction(_ *felt.Felt) (*core.Transaction, error) {
+	return nil, nil
+}
+
+func (f *fakeStarkNetData) Class(_ *felt.Felt) (*core.Class, error) {
 	return nil, nil
 }
